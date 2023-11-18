6d255d997fc3a77928401ccbc056cdc45e294dff
Dzmitry Kasinets <dmitryledger@gmail.com>
2023-11-18 01:11:58 -0500

    Starting adding a pipeline. Updated Readme

--- main.py
+++ main.py
@@ -167,8 +167,15 @@ def runCroppedOnlySpeedSigns():
 
 
 def main(debug):
-    print("\n")
-    # tmr = Timer() # Set timer
+    """
+        Pipeline: YOLO -> CNN1 -> CNN2 -> CNN3
+        - YOLO - Detect road signs: Detect bounding box coordinates (& classify into: prohibitory (ClassID: 0), danger (ClassID: 1), mandatory (ClassID: 2), other (ClassID: 3))
+        - CNN1 (Cropped only) - Classify into: prohibitory (ClassID: 0), danger (ClassID: 1), mandatory (ClassID: 2), other (ClassID: 3)
+        - CNN2 (Prohibitory signs only) - Classify into: any speed sign (ClassID: 999), no overtaking (trucks) (ClassID: 10), no overtaking (ClassID: 9), no traffic both ways (ClassID: 15), no trucks (ClassID: 16)
+        - CNN3 (Speed signs only) - Classify into: speed limit 50 (ClassID: 2), speed limit 70 (ClassID: 4), speed limit 30 (ClassID: 1), speed limit 120 (ClassID: 8), speed limit 80 (ClassID: 5), speed limit 100 (ClassID: 7), speed limit 60 (ClassID: 3), speed limit 20 (ClassID: 0)
+    """
+    print("\n Run Pipeline!")
+    tmr = Timer() # Set timer
     
     if debug:
         showDataSamples(DATA_DIR)
@@ -180,9 +187,9 @@ def main(debug):
     # runCroppedOnlyProhibitory()
 
     # CNN 3 - 8 Classes - Speed signs only 
-    runCroppedOnlySpeedSigns() 
+    # runCroppedOnlySpeedSigns() 
 
-    # tmr.ShowTime() # End timer.
+    tmr.ShowTime() # End timer.
 
 
 if __name__ == "__main__":
